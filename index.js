b2c08d32927bbade7edfeb0c6b481021b8aa3667
Abhimanyu Singh <superhit0@gmail.com>
2019-06-10 12:11:48 +0530

    Added tray icon & electron-webpack & electron-builder

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const electron = require('electron');
+const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray, nativeImage: NativeImage, systemPreferences } = electron;
 
 let mainWindow;
+let mainTray;
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 350,
+    height: 450,
+    titleBarStyle: 'hidden',
+    skipTaskbar: true,
+    resizable: false
+  });
+  mainWindow.setWindowButtonVisibility(false);
+};
+
+const createMainTray = () => {
+  // const nativeImage = NativeImage.createFromBuffer(new Buffer('HELLO'));
+  if(systemPreferences.isDarkMode()) {
+    mainTray = new Tray(path.join(process.cwd() + '/assets/spinner_dark.png'));
+  } else {
+    mainTray = new Tray(path.join(process.cwd() + '/assets/spinner_light.png'));
+  }
+};
+
+const updateMyAppTheme = ( isDarkMode ) => {
+
+};
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  createMainWindow();
+  createMainTray();
 });
+
+systemPreferences.subscribeNotification(
+  'AppleInterfaceThemeChangedNotification',
+  () => {
+    updateMyAppTheme(systemPreferences.isDarkMode())
+  }
+)
+

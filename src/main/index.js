320cbef1d590f992fafe9279423218357e634389
Abhimanyu Singh <superhit0@gmail.com>
2019-06-13 23:15:22 +0530

    Added logic to update time in app

--- src/main/index.js
+++ src/main/index.js
@@ -9,9 +9,49 @@ const windowBounds = {
   height: 450
 };
 
+const dateMap = date => [date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
+const timeBoundsMap = {
+  day: {
+    min:[1,1,1,0,0,0],
+    max:[1,1,2,0,0,0]
+  },
+  month: {
+    min:[1,1,0,0,0,0],
+    max:[1,2,0,0,0,0]
+  },
+  year: {
+    min:[1,0,0,0,0,0],
+    max:[2,0,0,0,0,0]
+  }
+};
+
+const getTimeBounds = (type, date) => {
+  const { min, max } = timeBoundsMap[type] || timeBoundsMap['day'];
+  const dateMapData = dateMap(date);
+
+  const [ minMap, maxMap ] = [ min, max ].map(it => (
+    it.map((value, index) => {
+      let offset = 0;
+      if (value > 1) {
+        offset = 1;
+        value = 1;
+      }
+      return value * dateMapData[index] + offset;
+    })
+  ));
+
+  return {
+    min: Reflect.construct(Date, minMap),
+    max: Reflect.construct(Date, maxMap)
+  }
+};
+
 const updateMyAppTheme = () => {
   const isDarkMode = systemPreferences.isDarkMode();
-  mainWindow.webContents.send('tray-data', '88', isDarkMode);
+  const currentTime = new Date();
+  const timeBounds = getTimeBounds('day', currentTime);
+  const timeSpent = Math.round((currentTime.getTime() - timeBounds.min) * 100 / (timeBounds.max - timeBounds.min));
+  mainWindow.webContents.send('tray-data', timeSpent, isDarkMode);
 };
 
 const toggleMainWindowVisiblibity = (event, bounds) => {

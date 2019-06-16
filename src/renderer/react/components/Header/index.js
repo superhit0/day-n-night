c7773a8da37a1b4810992ea049dca39c34e95c2c
Abhimanyu Singh <superhit0@gmail.com>
2019-06-16 17:21:14 +0530

    Added header tabs

--- src/renderer/react/components/Header/index.js
+++ src/renderer/react/components/Header/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export default function() {
-  return (
-    <div className="h-15 text-center">
-      Header Header Header
-    </div>
-  );
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="h-15 text-center container pt-1 pb-1">
+        <ul className="nav nav-pills nav-justified">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Month</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Day</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Year</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }

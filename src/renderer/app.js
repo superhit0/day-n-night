4ce9e9d87ed9e5765c8a0ebe1811c925a9f6483f
Abhimanyu Singh <superhit0@gmail.com>
2019-06-16 14:38:02 +0530

    Cleaned up config to use default settings of electron webpack

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './react';
 
+import '../../assets/bootstrap.css';
+
 render(<App />, document.getElementById('app'));

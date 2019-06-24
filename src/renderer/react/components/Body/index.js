ce9074257c0687948e03b4e2951ce5c9e9d27823
Abhimanyu Singh <superhit0@gmail.com>
2019-06-24 11:18:45 +0530

    Made circle on Body

--- src/renderer/react/components/Body/index.js
+++ src/renderer/react/components/Body/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function() {
   return (
-    <div className="h-74">
-      Body
+    <div className="h-74 p-2 pl-3 pr-3">
+      <div className="h-100 rounded-circle">
+        &nbsp;
+      </div>
     </div>
   );
 }

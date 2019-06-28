76d2d42b4c3b035b687047bbd7ff5728dcc2a647
Abhimanyu Singh <superhit0@gmail.com>
2019-06-28 11:47:50 +0530

    Added sytling for header

--- src/renderer/react/components/Header/index.js
+++ src/renderer/react/components/Header/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context';
+import './styles.scss';
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <div className="h-15 text-center container pt-1 pb-1">
-        <ul className="nav nav-pills nav-justified">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Month</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Day</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Year</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+export default () => {
+  const { theme } = useContext(AppContext);
+
+  return (
+    <div className="header h-15 text-center container pt-2 pb-1 mx-auto">
+      <ul className="nav nav-pills nav-justified">
+        <li className="nav-item">
+          <a className="nav-link" href="#">Month</a>
+        </li>
+        <li className="nav-item">
+          <a className={`nav-link active ${theme}`} href="#">Day</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Year</a>
+        </li>
+      </ul>
+    </div>
+  );
 }

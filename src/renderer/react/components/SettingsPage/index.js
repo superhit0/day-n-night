60dd88aa1636ada730da315fc797f4704868f227
Abhimanyu Singh <superhit0@gmail.com>
2019-07-01 12:20:55 +0530

    Made bg color of settings Page as light

--- src/renderer/react/components/SettingsPage/index.js
+++ src/renderer/react/components/SettingsPage/index.js
@@ -3,7 +3,7 @@ import Toggle from '../Toggle';
 
 export default function({ setShowSettingsPage }) {
   return (
-    <div className="container vh-100">
+    <div className="container vh-100 bg-light">
       <h1>Settings Page</h1>
       <Toggle/>
     </div>

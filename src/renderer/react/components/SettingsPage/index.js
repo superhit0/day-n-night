55e909a3c8fa916b7bc50dda5c424d81ab37f789
Abhimanyu Singh <superhit0@gmail.com>
2019-07-01 21:50:01 +0530

    Added TimePicker for settings page

--- src/renderer/react/components/SettingsPage/index.js
+++ src/renderer/react/components/SettingsPage/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import TimePicker from 'rc-time-picker';
 import Toggle from '../Toggle';
 import SettingsHeader from '../SettingsHeader';
 
+import 'rc-time-picker/assets/index.css';
+
 export default function({ setShowSettingsPage }) {
+  const format = 'H:mm';
+  const onChange = () => {};
   return (
     <div className="container vh-100 bg-light">
       <SettingsHeader setShowSettingsPage={setShowSettingsPage} />
       <hr className="mt-1" />
-      <Toggle/>
+      <div>
+        <label>Awake Mode</label>
+        <div className="float-right">
+          <Toggle />
+        </div>
+      </div>
+      <div className="mt-2 mb-1">
+        Awake Time
+      </div>
+      <div>
+        <TimePicker
+          showSecond={false}
+          className="w-48 px-1"
+          onChange={onChange}
+          format={format}
+          inputReadOnly
+        />
+        -
+        <TimePicker
+          showSecond={false}
+          className="w-48 px-1"
+          onChange={onChange}
+          format={format}
+          inputReadOnly
+        />
+      </div>
     </div>
   );
 }
